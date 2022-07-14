a1a48261d610460b7f0d41f2f5735e156b7d8d8f
matheus-bernat <matheus.bernat@gmail.com>
2022-07-14 05:15:39 -0300

    167: enums, import foundation.dart for @required, ListView.builder for efficiency in displaying list

--- meals_app/lib/category_meals_screen.dart
+++ meals_app/lib/category_meals_screen.dart
@@ -1,26 +1,28 @@
 ﻿import 'package:flutter/material.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
-  static const routeName = '/category-meals'; // can be accessed without instantiating class
+  static const routeName =
+      '/category-meals'; // can be accessed without instantiating class
 
   // final String categoryId;
   // final String categoryTitle;
 
   // CategoryMealsScreen(this.categoryId, this.categoryTitle);
 
-
   @override
   Widget build(BuildContext context) {
-
-    final routeArgs = ModalRoute.of(context).settings.arguments as Map<String, String>;
+    final routeArgs =
+        ModalRoute.of(context).settings.arguments as Map<String, String>;
     final categoryTitle = routeArgs['title'];
     final categoryiD = routeArgs['id'];
     return Scaffold(
       appBar: AppBar(
         title: Text(categoryTitle),
       ),
-      body: Center(
-        child: Text('The Recipes for the category'),
+      body: ListView.builder(
+        itemBuilder: (ctx, index) {
+          
+        }, itemCount: ,
       ),
     );
   }
